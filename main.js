faf970547343f8000f1dc005fd5a599071ad8a50
Rafli-irh <raflimuhamadirham@gmail.com>
2025-05-20 17:37:25 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-let angka = 5 ; // ganti dengan angka yang ingin dicek
+let angka = 7 ; 
 if (angka % 2 === 0) {console.log(angka + "adalah bilangan genap");}
 else {console.log(angka + " adalah bilangan ganjil");}
